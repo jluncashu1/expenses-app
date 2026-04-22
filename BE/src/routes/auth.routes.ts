41fa9320dd8f225fe9243983ba1f1fe9ac36aa82
jluncashu1 <eugenluncashu@gmail.com>
2026-04-22 16:57:46 +0300

    MG-001/login: Added login route

--- BE/src/routes/auth.routes.ts
+++ BE/src/routes/auth.routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controller.js";
+import { login, register } from "../controllers/auth.controller.js";
 
 const authRoutes = Router();
 
-authRoutes.get('/register', (req, res) => {
-    res.json('vasok')
-})
 authRoutes.post('/register', register);
+authRoutes.post('/login', login);
 
 export default authRoutes;
